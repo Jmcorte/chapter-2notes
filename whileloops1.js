a4e7ab8a50dd0c40269d963f6e1642d02ca361b5
6exf <6exf@fresnou.org>
2023-02-17 14:14:46 -0800

    Commit

---
+++ whileloops1.js
@@ -0,0 +1,20 @@
+const readLine = require('readline-sync');
+
+let y = -1;
+while (y< 25){
+    y++;
+    console.log(y);
+}
+
+let x = -1;
+while (x< 50){
+    x++;
+    console.log(x);
+}
+
+let z = -11;
+while (z< 10){
+    z++;
+    console.log(z);
+}
+
